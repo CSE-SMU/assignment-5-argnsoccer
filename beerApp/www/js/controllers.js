b3ab1aa4a5e32050e67c2e64f85703bc78725e8b
argnsoccer <amgras@smu.edu>
2016-02-29 22:12:23 -0600

    signifant work
    testing on other computer with ionic

--- beerApp/www/js/controllers.js
+++ beerApp/www/js/controllers.js
@@ -14,11 +14,16 @@ angular.module('starter.controllers', [])
       url: 'https://salty-taiga-88147.herokuapp.com/beers/',
       params: {
         name: $scope.form.name,
+        abv: $scope.form.abv,
+        hasLabels: $scope.form.labels,
+        year: $scope.form.year,
+        isOrganic: $scope.form.organic,
+
       }
       }).then(function successCallback(response) {
         $state.go('app.beers');
 
-        BeerData.data = response;
+        BeerData.data = response.data;
 
       }, function errorCallback(response) {
 
@@ -30,40 +35,13 @@ angular.module('starter.controllers', [])
   return {data: {}};
 })
 
-.factory('BeerData2', function(){
-  return {data: {}};
-})
+.controller('BeersCtrl', function($scope, $http, BeerData) {
 
-.controller('BeersCtrl', function($scope, $http, BeerData, BeerData2) {
-  for(i = 0; i < BeerData.data.data.totalResults; i++){
-    $scope.id = BeerData.data.data.data[i].id;
-    console.log($scope.id);
-    $http({
-      method: 'GET',
-      url: 'https://salty-taiga-88147.herokuapp.com/beers/',
-      params: {
-        id: $scope.id,
-      }
-      }).then(function successCallback(response) {
-
-        BeerData2.data = response;
-
-      }, function errorCallback(response) {
-
-      });
-     console.log(BeerData2.data.data.data[i].name);
-   }
-
-  $scope.beers = [
-    { title: BeerData.data.name, id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+  $scope.beers = BeerData.data.data;
 })
 
 .controller('BeerCtrl', function($scope, $stateParams, BeerData) {
   console.log($stateParams.id);
+  $scope.beers = BeerData.data.data;
+  $scope.sid = $stateParams.id;
 })
